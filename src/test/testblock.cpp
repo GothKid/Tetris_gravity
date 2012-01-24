fcf07cc8cd13eded4d2589768c45b618968d7051
winterdx <winterdx@gmail.com>
2012-01-24 01:12:06 +0100

    Added ISC license

--- src/test/testblock.cpp
+++ src/test/testblock.cpp
@@ -1,5 +1,21 @@
+/*Copyright (c) 2012, winterdx <winterdx@gmail.com>
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.*/
+
 #include "../block.h"
 #include <cstring>
+
+void fake_draw(std::list<Coord>, COLOR);
 
 class Block_test : public Block {
     public:
@@ -8,9 +24,11 @@ class Block_test : public Block {
         int y() {return _y;}
     private:
         const bool (*get_rotation_data())[SHAPE_SIDE*SHAPE_SIDE];
+        COLOR getcolor() const;
 };
 
-const bool (*Block_test::get_rotation_data())[SHAPE_SIDE*SHAPE_SIDE] {}
+const bool (*Block_test::get_rotation_data())[SHAPE_SIDE*SHAPE_SIDE] { return false; }
+COLOR Block_test::getcolor() const { return GREY; }
 Block_test::Block_test(int x, int y) : Block() {
     _x = x;
     _y = y;
@@ -42,10 +60,11 @@ int main() {
         return 1;
     }
 
-    block1->draw(TERM);
-    block2->draw(TERM);
+    block1->draw(fake_draw);
+    block2->draw(fake_draw);
     delete block1;
     delete block2;
     printf("Tests passed.\n");
     return 0;
 }
+void fake_draw(std::list<Coord>, COLOR) {}
--- src/test/testtetris.cpp
+++ src/test/testtetris.cpp
@@ -1,4 +1,20 @@
+/*Copyright (c) 2012, winterdx <winterdx@gmail.com>
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.*/
+
 #include "../game.h"
+
+void console_draw(std::list<Coord>, COLOR);
 
 int main() {
     std::list<Rectangle*> rectangles;
@@ -10,14 +26,14 @@ int main() {
         fprintf(stderr, "Error creando bloque nuevo\n");
         return 1;
     }
-    bl1->draw(TERM);
+    bl1->draw(console_draw);
     printf("\n");
     bl1->rotate();
-    bl1->draw(TERM);
+    bl1->draw(console_draw);
     printf("\n");
     for (int i = 0; i < 30; i++)
         advance_blocks(blocks);
-    bl1->draw(TERM);
+    bl1->draw(console_draw);
     printf("\n");
 
     Block *bl2 = new Line_block;
@@ -26,24 +42,29 @@ int main() {
         return 1;
     }
     blocks.push_back(bl2);
-    bl2->draw(TERM);
+    bl2->draw(console_draw);
     printf("\n");
     bl2->rotate();
     bl2->move_right();
     bl2->move_right();
     bl2->move_right();
     bl2->move_right();
-    bl2->draw(TERM);
+    bl2->draw(console_draw);
     printf("\n");
     for (int i = 0; i < 30; i++)
         advance_blocks(blocks);
-    bl2->draw(TERM);
+    bl2->draw(console_draw);
     printf("\n");
 
     clean_rows(blocks);
-    bl2->draw(TERM);
+    bl2->draw(console_draw);
 
     delete_empty_blocks(blocks);
 
     return 0;
+}
+
+void console_draw(std::list<Coord> coords, COLOR c) {
+    for (std::list<Coord>::const_iterator i = coords.begin(); i != coords.end(); ++i)
+        fprintf(stderr, "Occupied at: %d,%d\n", i->x, i->y);
 }
